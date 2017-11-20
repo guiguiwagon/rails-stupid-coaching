e0af1c08d8c2d7e5fa419c81ba2db06393c8af18
guillaume lestrade <guillaumelestrade@gmail.com>
2017-11-20 16:19:30 +0100

    hi

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  # get 'questions/ask'
+  # get 'questions/answer'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
